71fe38706d02efac7532329802652786f8940676
Zachary King <zak@delfina.com>
2025-04-28 15:42:08 -0700

    add version info to the footer

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,23 @@
+import fs from "fs";
 import type { NextConfig } from "next";
+
+// https://stackoverflow.com/questions/34518389/get-hash-of-most-recent-git-commit-in-node
+let gitSha = null;
+const rev = fs.readFileSync("../.git/HEAD").toString().trim();
+if (rev.indexOf(":") === -1) {
+  gitSha = rev;
+} else {
+  gitSha = fs
+    .readFileSync("../.git/" + rev.substring(5))
+    .toString()
+    .trim();
+}
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  env: {
+    NEXT_PUBLIC_GIT_SHA: gitSha.slice(0, 7),
+  },
 };
 
 export default nextConfig;
