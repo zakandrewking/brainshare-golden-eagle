e437df2346bbd62c6c305b8dd16d036ff19a4f35
Zachary King <zak@delfina.com>
2025-04-27 19:53:47 -0700

    drizzle for db migrations

---
+++ db/README.md
@@ -0,0 +1,7 @@
+# dev workflow
+
+Make edits to /db/schema/*.ts, and then run to sync with the local db:
+
+```bash
+bunx drizzle-kit push
+```
---
+++ db/schema/documents.ts
@@ -0,0 +1,5 @@
+import { integer, pgTable } from 'drizzle-orm/pg-core';
+
+export const documents = pgTable("documents", {
+  id: integer(),
+});
