d92eceb58bbe1486cc1ae7c29e9558b4997651ef
Zachary King <zak@delfina.com>
2025-04-28 22:58:58 -0700

    add a check constraint to the documents table

--- db/schema/documents.ts
+++ db/schema/documents.ts
@@ -1,12 +1,20 @@
+import { sql } from 'drizzle-orm';
 import {
+  check,
   integer,
   pgTable,
   text,
 } from 'drizzle-orm/pg-core';
 
-export const documents = pgTable("document", {
-  id: integer().primaryKey().generatedByDefaultAsIdentity(),
-  title: text("title").notNull(),
-  liveblocks_id: text("liveblocks_id").notNull(),
-  type: text("type").notNull(),
-});
+export const documents = pgTable(
+  "document",
+  {
+    id: integer().primaryKey().generatedByDefaultAsIdentity(),
+    title: text("title").notNull(),
+    liveblocks_id: text("liveblocks_id").notNull(),
+    type: text("type").notNull(),
+  },
+  (table) => [
+    check("document_type_check", sql`${table.type} IN ('text', 'table')`),
+  ]
+);
