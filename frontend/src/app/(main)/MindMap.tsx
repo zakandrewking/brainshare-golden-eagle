ac22c5d96e4d9dda976906237add5a069b5f7268
Zachary King <zak@delfina.com>
2025-05-01 08:58:41 -0700

    dark mode

--- frontend/src/app/(main)/MindMap.tsx
+++ frontend/src/app/(main)/MindMap.tsx
@@ -1,8 +1,15 @@
+"use client";
+
 import "@xyflow/react/dist/style.css";
 
-import React from "react";
+import React, {
+  useEffect,
+  useState,
+} from "react";
 
-import { ReactFlow } from "@xyflow/react";
+import { useTheme } from "next-themes";
+
+import { ColorMode, ReactFlow } from "@xyflow/react";
 
 const initialNodes = [
   { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
@@ -11,9 +18,19 @@ const initialNodes = [
 const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
 
 export default function MindMap() {
+  const { theme } = useTheme();
+  // use state for the colorMode to avoid hydration errors
+  const [colorMode, setColorMode] = useState<ColorMode>("light");
+  useEffect(() => {
+    setColorMode((theme || "light") as ColorMode);
+  }, [theme]);
   return (
     <div className="w-[1000px] h-[1000px]">
-      <ReactFlow nodes={initialNodes} edges={initialEdges} />
+      <ReactFlow
+        nodes={initialNodes}
+        edges={initialEdges}
+        colorMode={colorMode}
+      />
     </div>
   );
 }
