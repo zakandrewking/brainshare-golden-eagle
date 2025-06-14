ac583d92090f104e6b88090867b1905e039d8e9f
Zachary King <zak@delfina.com>
2025-06-13 20:16:39 -0700

    serve with a s3 bucket

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -1,7 +1,46 @@
-import * as aws from '@pulumi/aws';
+import * as aws from "@pulumi/aws";
+import * as pulumi from "@pulumi/pulumi";
 
-// chunks bucket
-const c = new aws.s3.Bucket("brainshare-primary", {});
+const ySweet = new aws.s3.Bucket("brainshare-y-sweet", {
+  bucket: "brainshare-y-sweet-d7bac3e",
+});
 
-// files bucket
-const f = new aws.s3.Bucket("brainshare-files", {});
+const user = new aws.iam.User("ySweetUser", {
+  name: "ysweet-s3-user",
+});
+
+const policy = new aws.iam.Policy("ySweetBucketPolicy", {
+  policy: ySweet.arn.apply((bucketArn) =>
+    JSON.stringify({
+      Version: "2012-10-17",
+      Statement: [
+        {
+          Effect: "Allow",
+          Action: ["s3:ListBucket"],
+          Resource: bucketArn,
+        },
+        {
+          Effect: "Allow",
+          Action: ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"],
+          Resource: `${bucketArn}/*`,
+        },
+      ],
+    })
+  ),
+});
+
+new aws.iam.UserPolicyAttachment("ySweetUserPolicyAttachment", {
+  user: user.name,
+  policyArn: policy.arn,
+});
+
+const accessKey = new aws.iam.AccessKey("ySweetAccessKey", {
+  user: user.name,
+});
+
+export const ySweetAccessKeyId = accessKey.id;
+export const ySweetSecretAccessKey = accessKey.secret;
+export const ySweetBucketName = ySweet.bucket;
+export const awsRegion = aws.config.region;
+export const ySweetS3Url = pulumi.interpolate`s3://${ySweet.bucket}/`;
+// to retrieve the key `pulumi stack output --show-secrets`
