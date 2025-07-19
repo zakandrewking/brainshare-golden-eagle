f4f5dfa684dc3ee79675d918ec3abafa54b81b59
Zachary King <zak@delfina.com>
2025-07-19 10:14:48 -0700

    chat list stub

--- frontend/src/app/(main)/chat/page.tsx
+++ frontend/src/app/(main)/chat/page.tsx
@@ -1,9 +1,11 @@
 import Chat from "@/components/blocks/chat/chat";
+import ChatList from "@/components/blocks/chat/chat-list";
 import Container from "@/components/ui/container";
 
 export default function ChatPage() {
   return (
     <Container>
+      <ChatList />
       <Chat />
     </Container>
   );
