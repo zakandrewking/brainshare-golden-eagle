be08796349bb6ddaeac1f346b7dce3b414b5d154
Zachary King <zak@delfina.com>
2025-07-20 09:08:12 -0700

    inngest test logs

--- frontend/src/app/inngest-test-server.tsx
+++ frontend/src/app/inngest-test-server.tsx
@@ -3,10 +3,12 @@
 import { inngest } from "@/inngest/client";
 
 export async function inngestTest() {
-  await inngest.send({
+  console.log("inngestTest");
+  const result = await inngest.send({
     name: "test/hello.world",
     data: {
       email: "testUser@example.com",
     },
   });
+  console.log("inngestTest result", result);
 }
